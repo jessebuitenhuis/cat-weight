6d5cd9c67b62724e7d7e5fc6596d680d0e05a494
Jesse Buitenhuis <jessebuitenhuis@gmail.com>
2023-11-11 13:18:25 +0100

    format on pre-commit

--- apps/cat-weight/src/app/cat/cat.store.spec.ts
+++ apps/cat-weight/src/app/cat/cat.store.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
-import { CatStore } from './cat.store';
 import { provideMockStore } from '@cat-weight/util/data';
+import { CatStore } from './cat.store';
 
 beforeEach(() =>
   TestBed.configureTestingModule({
